d1ab12f3c24d645e65195f13dde89171b2d5037d
chupengfei <450793585@qq.com>
2023-12-11 20:47:27 +0800

    first commit

---
+++ rspress.config.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: '褚鹏飞的博客',
+  description: '褚鹏飞的博客',
+  icon: "/logo.jpeg",
+  logo: {
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/MagicalBridge' },
+    ],
+  },
+});
